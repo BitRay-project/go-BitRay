3a5b6fd048f009c908666324effdc34437975e10
yolkCat <80354983+yolkCat@users.noreply.github.com>
2021-04-25 23:37:54 +0300

    Update denomination.go

--- params/denomination.go
+++ params/denomination.go
@@ -1,28 +1,28 @@
-// Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// Copyright 2021 The go-BitRay Authors
+// This file is part of the go-BitRay library.
 //
-// The go-ethereum library is free software: you can redistribute it and/or modify
+// The go-BitRay library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The go-ethereum library is distributed in the hope that it will be useful,
+// The go-BitRay library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+// along with the go-BitRay library. If not, see <http://www.gnu.org/licenses/>.
 
 package params
 
-// These are the multipliers for ether denominations.
-// Example: To get the wei value of an amount in 'gwei', use
+// These are the multipliers for BTRC denominations.
+// Example: To get the Rb value of an amount in 'grb', use
 //
-//    new(big.Int).Mul(value, big.NewInt(params.GWei))
+//    new(big.Int).Mul(value, big.NewInt(params.GRb))
 //
 const (
-	Wei   = 1
-	GWei  = 1e9
-	Ether = 1e18
+	Rb   = 1
+	GRb  = 1e9
+	BTRC = 1e18
 )
