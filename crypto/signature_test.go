b430558e1c7f71a5a1997c16a7a07889741fa40e
yolkCat <80354983+yolkCat@users.noreply.github.com>
2021-04-25 17:22:28 +0300

    Update signature_test.go

--- crypto/signature_test.go
+++ crypto/signature_test.go
@@ -22,9 +22,9 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/BitRay-project/go-BitRay/common"
+	"github.com/BitRay-project/go-BitRay/common/hexutil"
+	"github.com/BitRay-project/go-BitRay/common/math"
 )
 
 var (
