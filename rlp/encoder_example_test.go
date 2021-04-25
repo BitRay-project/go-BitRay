0e0173dabbedc54ce3044fc0cdfa31387bcfeaed
yolkCat <80354983+yolkCat@users.noreply.github.com>
2021-04-25 17:37:07 +0300

    add rlp dependencies

---
+++ rlp/encoder_example_test.go
@@ -0,0 +1,46 @@
+// Copyright 2021 The go-BitRay Authors
+// This file is part of the go-BitRay library.
+//
+// The go-BitRay library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-BitRay library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-BitRay library. If not, see <http://www.gnu.org/licenses/>.
+
+package rlp
+
+import (
+	"fmt"
+	"io"
+)
+
+type MyCoolType struct {
+	Name string
+	a, b uint
+}
+
+// EncodeRLP writes x as RLP list [a, b] that omits the Name field.
+func (x *MyCoolType) EncodeRLP(w io.Writer) (err error) {
+	return Encode(w, []uint{x.a, x.b})
+}
+
+func ExampleEncoder() {
+	var t *MyCoolType // t is nil pointer to MyCoolType
+	bytes, _ := EncodeToBytes(t)
+	fmt.Printf("%v → %X\n", t, bytes)
+
+	t = &MyCoolType{Name: "foobar", a: 5, b: 6}
+	bytes, _ = EncodeToBytes(t)
+	fmt.Printf("%v → %X\n", t, bytes)
+
+	// Output:
+	// <nil> → C0
+	// &{foobar 5 6} → C20506
+}
